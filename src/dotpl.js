8c11a4e3c8ee53f7123762a298cdb646837a843d
zzzhan <imailer@sina.com>
2016-12-17 20:32:05 +0800

    support locate value which key starts with '../' or '/'

--- src/dotpl.js
+++ src/dotpl.js
@@ -12,20 +12,31 @@
 	var _tag = 'tpl';
 	var _topener = '<';
 	var _tcloser = '>';
-	function _diving(key,kv) {
-		var keys = key.split("\.");
-		var i = 0;
+	function _diving(key,kv, renderer, pk, parent,root) {
+		var ret;
+		if(key.indexOf('/')===0) {
+		  key = key.substring(1);
+		  ret = root||kv;
+		} else if(key.indexOf('../')===0) {
+		  key = key.substring(3);
+		  ret = parent||kv;
+		} else {
+		  ret = kv;
+		}
+		var keys = key.split("\."),i = 0;
 		do {
-			kv = kv[keys[i++]];
-			if(kv==null) {break;}
-		} while(i<keys.length&&typeof(kv)==='object');
-		return kv;
+			ret = ret[keys[i++]];
+			if(ret==null) {break;}
+		} while(i<keys.length&&typeof(ret)==='object');	
+		if(typeof ret==='string') {
+		  return _applyMapTpl(ret, kv, renderer, pk, parent,root);
+		} else { return ret;}
 	}
-	function _applyMapTpl(tpl, values, renderer, pk, parent) {	
+	function _applyMapTpl(tpl, values, renderer, pk, parent,root) {	
 		var re = new RegExp(_opener+'([^'+_closer+']+?)'+_closer, 'ig');///\$\{([^\}]+?)\}/ig;
 		var view = tpl.replace(re, function($0,$1) {
 			try {
-				var val = _diving($1,values);
+				var val = _diving($1,values, renderer, pk, parent,root);
 				val = (val==null?"":val);
 				if(typeof val==='boolean') {return val;}
 				var func = null;
@@ -73,10 +84,13 @@
 		} catch(e){ throw e;}
 		return xmlhttp;
 	}
-	function _applyTpl(tpl, data, renderer, pk, parent){
+	function _applyTpl(tpl, data, renderer, pk, parent, root){
 		//var regx = /<(tpl\d?)\s+(\w+)\s*=\s*(['|"]{1})([^\3]+?)\3\s*>([\s\S]+?)<\/\1>/ig;
 		//var __regx = /<(tpl\d?)\s+([^>]+?)>([\s\S]+?)<\/\1>/ig;
 		var __regx = new RegExp('\\'+_topener+'('+_tag+'\\d?)\\s+([^\\'+_tcloser+']+?)\\'+_tcloser+'([\\s\\S]+?)\\'+_topener+'\\/\\1\\'+_tcloser, 'ig');
+		if(!root) {
+		   root = data;	
+		}
 		if(__regx.test(tpl)) {
 			tpl = tpl.replace(__regx, function($0,$1,$2,$3){
 				var output = "";
@@ -91,7 +105,7 @@
 					if(forkey!=null) {
 						var arr = data;
 						if(forkey!=='.') {
-							arr = _diving(forkey,data);
+							arr = _diving(forkey,data, renderer, pk, parent,root);
 						}
 						if(typeof renderer==='object') {
 							if(!!renderer.beforeLoop) {
@@ -112,20 +126,20 @@
 										continue;
 									}
 								}
-								output+=_applyTpl($3,item,renderer,forkey,data);
+								output+=_applyTpl($3,item,renderer,forkey,data,root);
 							}
 						} else {
 			                if(attr['emptytext']!=null) {output = attr['emptytext'];}
 						}
 					} else if(attr['if']!=null) {
 						try {
-							var strflag = _applyTpl(attr['if'],data,renderer, pk, parent);
+							var strflag = _applyTpl(attr['if'],data,renderer, pk, parent,root);
 							if(typeof strflag==='string') {
 								/*jslint evil: true */
 								strflag = eval('Boolean('+strflag+')');
 							}
 							if(strflag) {
-								return _applyTpl($3, data, renderer, pk, parent);
+								return _applyTpl($3, data, renderer, pk, parent,root);
 							} else {
 				                if(attr['emptytext']!=null) {output = attr['emptytext'];}
 							}
@@ -137,7 +151,7 @@
 				return output;         
 			});
 		}
-		return _applyMapTpl(tpl, data, renderer, pk, parent);
+		return _applyMapTpl(tpl, data, renderer, pk, parent,root);
 	}
 	var dotpl = {
 		diving:_diving,
--- test.js
+++ test.js
@@ -1,4 +1,27 @@
-var dotpl = require('./dist/dotpl.min');
+var dotpl = require('./src/dotpl');
 console.log(dotpl.applyTpl("hello ${data.val}", {data:{val:"world"}}));
-dotpl.setDelimiters('<%', '%>');
-console.log(dotpl.applyTpl("hello <%data.val%>", {data:{val:"world"}}));
+dotpl.setDelimiters('{{', '}}');
+console.log(dotpl.applyTpl("hello {{data.val}}", {data:{val:"world"}}));
+console.log(dotpl.applyTpl("hello <tpl for='shapes_group'>{{name}}\n<tpl0 for='group'>{{type}}-{{name}}\n</tpl0></tpl>", {"name":"Root Name","shapes_group":[{
+  "name":"Equation {{test}}",
+  "test":"testing",
+  "group":[{
+    "type":"plus",
+    "name":"Plus {{../test}}"
+  },{
+    "type":"minus",
+    "name":"Minus {{/name}}"
+  },{
+    "type":"multiply",
+    "name":"Multiply"
+  },{
+    "type":"division",
+    "name":"Division"
+  },{
+    "type":"equal",
+    "name":"Equal"
+  },{
+    "type":"notequal",
+    "name":"Notequal"
+  }]
+}]}));
